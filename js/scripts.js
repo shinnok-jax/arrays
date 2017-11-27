88c6d50b73192a1405be13f8ddfeb9b31b8cf3b5
shinok-jax <jax.shinnok@gmail.com>
2017-11-27 09:59:42 +0100

    poprawka

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 var newName = 'Marian'
-if (allNames.indexOf(newName) === -1.) {
+if (allNames.indexOf(newName) === -1) {
   allNames.push(newName);
 }
